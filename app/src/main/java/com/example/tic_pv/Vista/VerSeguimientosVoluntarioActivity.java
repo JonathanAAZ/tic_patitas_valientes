b3f363fdb9f4bf151d48067d46ddfa3198d304b0
Jonathan Andrade <jonathan.andrade@unl.edu.ec>
2026-01-22 00:21:56 -0500

    Agregar XML para Seguimientos

---
+++ app/src/main/java/com/example/tic_pv/Vista/VerSeguimientosVoluntarioActivity.java
@@ -0,0 +1,26 @@
+package com.example.tic_pv.Vista;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.tic_pv.R;
+
+public class VerSeguimientosVoluntarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_ver_seguimientos_voluntario);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
